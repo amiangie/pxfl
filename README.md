98c390ef4efec70d6a84b90aea11f191417f2259
Angie K. Titova <angie.badfox@gmail.com>
2017-06-12 00:38:25 +0300

    [General] Add inital styles, templates, icon font, image assets, dev files

---
+++ README.md
@@ -0,0 +1,3 @@
+# pxfl
+
+A project generated with [Panache](https://github.com/noel-noel/generator-panache).
---
+++ src/scripts/app.js
@@ -0,0 +1,8 @@
+/*
+  Project: pxfl
+  Author: angie
+ */
+
+var greet = require('./greeting.js')
+
+greet('World');
