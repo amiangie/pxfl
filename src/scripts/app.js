6312f6e5fac38a9654627a605b05fa00028b5b14
Angie K. Titova <angie.badfox@gmail.com>
2017-06-17 00:38:08 +0300

    [JS] Add tab functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -20,4 +20,14 @@
       }
     });
     
+    $('.js-tab').click(function(){  
+      $('.js-tab').attr('aria-selected', 'false');
+      $(this).attr('aria-selected', 'true');
+      $('.js-tabpanel').attr('aria-hidden', 'true');
+
+      const tabPanelId = $(this).attr('aria-controls');
+      const $tabPanel = $("#" + tabPanelId);  
+      $tabPanel.attr('aria-hidden', 'false');
+    });
+    
   });
